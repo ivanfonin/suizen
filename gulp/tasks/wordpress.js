dff18cda4c7ab771961a0392d3e02746f235d84d
Ivan Fonin <mail@ivanfonin.com>
2017-12-11 13:46:30 +0300

    Changed build to dist directory for production taks.

--- gulp/tasks/wordpress.js
+++ gulp/tasks/wordpress.js
@@ -8,47 +8,47 @@ var gulp = require('gulp'),
 // Copy php files.
 gulp.task('php', () => {
     return gulp.src(config.files.php.src)
-        .pipe(gulp.dest(config.project.build))
+        .pipe(gulp.dest(config.project.dist))
 })
 
 // Copy language files.
 gulp.task('languages', function() {
     return gulp.src(config.files.languages.src)
-        .pipe(gulp.dest(config.folders.languages.build))
+        .pipe(gulp.dest(config.folders.languages.dist))
 })
 
 // Copy images.
 gulp.task('images', function() {
     return gulp.src(config.files.images.src)
-        .pipe(gulp.dest(config.folders.images.build))
+        .pipe(gulp.dest(config.folders.images.dist))
 })
 
 // Copy video files.
 gulp.task('videos', function() {
     return gulp.src(config.files.videos.src)
-        .pipe(gulp.dest(config.folders.videos.build))
+        .pipe(gulp.dest(config.folders.videos.dist))
 })
 
 // Copy screenshot.
 gulp.task('screenshot', function() {
     return gulp.src(config.images.screenshot.src)
-        .pipe(gulp.dest(config.project.build))
+        .pipe(gulp.dest(config.project.dist))
 })
 
 // Copy javascript files.
 gulp.task('js', function() {
     return gulp.src(config.files.js.src)
-        .pipe(gulp.dest(config.folders.js.build))
+        .pipe(gulp.dest(config.folders.js.dist))
 })
 
 gulp.task('fonts', function() {
     return gulp.src(config.files.fonts.src)
-        .pipe(gulp.dest(config.folders.fonts.build))
+        .pipe(gulp.dest(config.folders.fonts.dist))
 })
 
 gulp.task('admin-css', ['fonts'], function() {
     return gulp.src(config.files.css.src)
-        .pipe(gulp.dest(config.folders.css.build))
+        .pipe(gulp.dest(config.folders.css.dist))
 })
 
 gulp.task('scss', ['admin-css'], () => {
@@ -66,7 +66,7 @@ gulp.task('scss', ['admin-css'], () => {
 gulp.task('css', ['scss'], () => {
     return gulp.src([config.project.src + 'style.css', config.project.temp + 'style.css'])
         .pipe(plugins.concat('style.css'))
-        .pipe(gulp.dest(config.project.build))
+        .pipe(gulp.dest(config.project.dist))
 })
 
 gulp.task('wordpress', ['php', 'languages', 'images', 'videos', 'screenshot', 'js', 'css'])
