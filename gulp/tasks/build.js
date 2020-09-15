73389dd65e1102ffa97e4b0b054d233c5ce16cc3
ivanfonin <ivanfonin@yandex.ru>
2020-09-15 13:39:15 +0300

    Modified build task according to modern theme development standarts.

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,15 +1,16 @@
 'use strict';
 
-var gulp = require( 'gulp' ),
-    sourcemaps = require( 'gulp-sourcemaps' ),
-    autoprefixer = require( 'autoprefixer' ),
-    sass = require( 'gulp-sass' ),
-    postcss = require( 'gulp-postcss' ),
-    cleancss = require( 'gulp-clean-css' ),
-    uglify = require( 'gulp-uglify' ),
-    rename = require( 'gulp-rename' ),
-    concat = require( 'gulp-concat' ),
-    config = require( '../config.js' );
+var gulp            = require( 'gulp' ),
+    sourcemaps      = require( 'gulp-sourcemaps' ),
+    autoprefixer    = require( 'autoprefixer' ),
+    sass            = require( 'gulp-sass' ),
+    postcss         = require( 'gulp-postcss' ),
+    cleancss        = require( 'gulp-clean-css' ),
+    uglify          = require( 'gulp-uglify' ),
+    plumber         = require( 'gulp-plumber' ),
+    rename          = require( 'gulp-rename' ),
+    concat          = require( 'gulp-concat' ),
+    config          = require( '../config.js' );
 
 // Copy php files.
 gulp.task( 'readme', () => {
@@ -50,6 +51,7 @@ gulp.task( 'screenshot', () => {
 // Process and copy javascript files.
 gulp.task( 'js', () => {
     return gulp.src( config.files.js.src )
+        .pipe( plumber() )
         // Copy unminified scripts.
         .pipe( gulp.dest( config.folders.js.build ) )
         // Create minified scripts for production.
@@ -60,35 +62,44 @@ gulp.task( 'js', () => {
         .pipe( gulp.dest( config.folders.js.build ) );
 } );
 
+// Copy font files.
 gulp.task( 'fonts', () => {
     return gulp.src( config.files.fonts.src )
         .pipe( gulp.dest( config.folders.fonts.build ) );
 } );
 
-gulp.task( 'admin-css', () => {
-    return gulp.src( config.files.css.src )
+// Copy print.css file.
+gulp.task( 'print-css', ( done ) => {
+    return gulp.src( config.files.scss.printCss )
+        .pipe( gulp.dest( config.project.build ) );
+} );
+
+// Build editor style blocks CSS.
+gulp.task( 'editor-style-blocks', () => {
+    return gulp.src( config.files.scss.editorStyleBlocks )
+        .pipe( sourcemaps.init() )
+        .pipe( sass() ).on( 'error', sass.logError )
+        .pipe( postcss( [ autoprefixer() ] ) )
+        // .pipe( cleancss() )
+        .pipe( sourcemaps.write() )
         .pipe( gulp.dest( config.folders.css.build ) );
 } );
 
+// Buils theme CSS.
 gulp.task( 'scss', () => {
     return gulp.src( config.files.scss.src )
         .pipe( sourcemaps.init() )
         .pipe( sass() ).on( 'error', sass.logError )
         .pipe( postcss( [ autoprefixer() ] ) )
-        .pipe( cleancss() )
+        // .pipe( cleancss() )
         .pipe( sourcemaps.write() )
-        .pipe( gulp.dest( config.project.temp ) );
-} );
-
-gulp.task( 'css', () => {
-    return gulp.src( [ config.project.src + 'style.css', config.project.temp + 'style.css' ] )
-        .pipe( concat( 'style.css' ) )
         .pipe( gulp.dest( config.project.build ) );
 } );
 
+// Build task.
 gulp.task( 'build',
     gulp.series(
-        'admin-css', 'scss', 'css',
+        'print-css', 'editor-style-blocks', 'scss',
         gulp.parallel( 'readme', 'php', 'languages', 'images', 'videos', 'screenshot', 'js', 'fonts' )
     )
 );
